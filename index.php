a77540015144bb8fd0818e605fcf466a0e7774d6
cotonea <cotonea@B4-W7-12.sio.chaptalqper>
2017-03-22 15:55:30 +0100

    fin partie II

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
-<?php 
+<?php
 
 require 'Modele.php';
 
-$billets = getBillets();
-
-require 'vueAccueil.php';
+try {
+  $billets = getBillets();
+  require 'vueAccueil.php';
+}
+catch (Exception $e) {
+  $msgErreur = $e->getMessage();
+  require 'vueErreur.php';
+}
